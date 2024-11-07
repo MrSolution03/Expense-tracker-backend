9877eb5609e9acf39802693ec7b9904a98c0c0c4
Onpointdev01 <onpointdev0@gmail.com>
2024-11-07 22:36:44 +0200

    setting setup

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -13,7 +13,7 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-ix%094l2&nj*hu@au6uc_jc+ai7&p($iuh*x6e7!7sb&=e80of'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['expensetracker.render.com', 'localhost']
 
